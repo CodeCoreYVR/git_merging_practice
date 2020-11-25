7c03eed04a8098df06add8df56c20c185f9d00d0
KenChukanit <chonlasek.c@gmail.com>
2020-11-25 12:01:48 -0800

    fix development.js

--- development.js
+++ development.js
@@ -1,6 +1,8 @@
+
 console.log('add some more code in dev')
 
 console.log('made some changes from master branch')
 console.log('apples')
 
 console.log('add stuff from merge_conflicts')
+console.log('Hello')
